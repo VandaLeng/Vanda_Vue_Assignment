b13287bff9243c332f387464b22daa1e85247f1b
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-06-18 10:26:03 +0700

    feat(feature/structure): added folder project vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="/src/assets/Vespa.jpg" alt="Logo" class="w-24 h-24 m-auto mt-6" />
+
+    <h1 class="text-center font-semibold bg-amber-200 w-400 m-auto p-4">
+      Hello world
+    </h1>
+
+    <WelcomeItemVue />
+  </div>
+</template>
+
+<script setup>
+import WelcomeItemVue from "./components/Todo.vue";
+</script>
+
+<style scoped>
+</style>
